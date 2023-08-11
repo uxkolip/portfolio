59d1938e4206cb59ca098430bea41495ab471556
Bill K <hello@oneclickmore.org>
2023-08-11 12:40:36 +0300

    css

---
+++ dist/js/about.js
@@ -0,0 +1,71 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+function checkiftooltip() {
+    $(window).width() > 768 ? ($('[data-toggle="tooltip"]').tooltip(), 
+    $("[data-toggle='tooltip']").tooltip(),
+    $('[data-toggle="tooltip"]').tooltip("enable")) : $('[data-toggle="tooltip"]').tooltip("disable")
+}
+
+$(document).ready(function() {
+    checkiftooltip();
+}); 
+
+$(window).resize(function() {
+    checkiftooltip();
+});
+
+    const lenis = new Lenis()
+
+    lenis.on('scroll', (e) => {
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        lenis.scrollTo(this.getAttribute('href'))
+      });
+    })
+
+    document.querySelectorAll('[data-target^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        lenis.scrollTo(this.getAttribute('data-target'))
+      });
+    })
+
+    $(window).scroll(function() {
+        $(this).scrollTop() > 800 ? $("#toTop").fadeIn("fast", function() {}) : $("#toTop").fadeOut("fast")
+    });
+
+
+
+const showAnim = gsap.from('.main-navbar', { 
+    yPercent: -100,
+    paused: true,
+    duration: 0.3
+}).progress(1);
+
+ScrollTrigger.create({
+    start: "top top",
+    end: 99999,
+    onUpdate: (self) => {
+        self.direction === -1 ? showAnim.play() : showAnim.reverse()
+    }
+});
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.to('progress', {
+  value: 100,
+  ease: 'none',
+  scrollTrigger: { scrub: 0.3 }
+});
+
+
---
+++ src/js/about.js
@@ -0,0 +1,80 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+function checkiftooltip() {
+    $(window).width() > 768 ? ($('[data-toggle="tooltip"]').tooltip(), 
+    $("[data-toggle='tooltip']").tooltip(),
+    $('[data-toggle="tooltip"]').tooltip("enable")) : $('[data-toggle="tooltip"]').tooltip("disable")
+}
+
+$(document).ready(function() {
+    checkiftooltip();
+}); 
+
+$(window).resize(function() {
+    checkiftooltip();
+});
+
+//smooth scroll
+    const lenis = new Lenis()
+
+    lenis.on('scroll', (e) => {
+      //console.log(e)
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        lenis.scrollTo(this.getAttribute('href'))
+      });
+    })
+
+    document.querySelectorAll('[data-target^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        lenis.scrollTo(this.getAttribute('data-target'))
+      });
+    })
+
+    //scroll toTop
+    $(window).scroll(function() {
+        $(this).scrollTop() > 800 ? $("#toTop").fadeIn("fast", function() {}) : $("#toTop").fadeOut("fast")
+    });
+    //scroll toTop end
+    
+//smooth scroll end
+
+
+//navbar show on scroll up
+const showAnim = gsap.from('.main-navbar', { 
+    yPercent: -100,
+    paused: true,
+    duration: 0.3
+}).progress(1);
+
+ScrollTrigger.create({
+    start: "top top",
+    end: 99999,
+    onUpdate: (self) => {
+        self.direction === -1 ? showAnim.play() : showAnim.reverse()
+    }
+});
+//navbar show on scroll up end
+
+// page progress
+gsap.registerPlugin(ScrollTrigger);
+gsap.to('progress', {
+  value: 100,
+  ease: 'none',
+  scrollTrigger: { scrub: 0.3 }
+});
+// page progress end
+
+
