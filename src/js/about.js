ba0b0a42561d9a67fc00efb11c85f11846d04a9d
Vasilis K <icemangoa@hotmail.com>
2023-09-30 22:20:47 +0300

    - added hotjar - animated the workspace section - various improvements

--- src/js/about.js
+++ src/js/about.js
@@ -173,42 +173,254 @@ timeline
 //homepage animations end
 
 
-
-
 // workspaces scroller
-/*gsap.registerPlugin(ScrollTrigger);
-
-const cards = $(".design-card");
+/*const cards = document.querySelectorAll(".deskImg");
 
 gsap.set(cards, {
-    position: "absolute",
-    opacity: 1,
-    scale: .1,
-    yPercent: 100,
-    xPercent: 0,
-    rotate: 0,
-    filter: 'blur(10px)'
+  position: 'absolute',
+  opacity: 0,
+  filter: 'blur(4px)',
+  yPercent: 0,
+  xPercent: 0
 });
 
-gsap.to(".design-card", {
-    yPercent: 0,
-    xPercent: 0,
-    stagger: 1000,
-    duration: 1400,
+const cardTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.deskContainer',
+    pin: true,
+    start: "center center",
+    end: "+=300%", // Adjust the end value for a longer animation
+    scrub: true
+  }
+});
+
+cards.forEach((card, index) => {
+  var currentCard = `${index + 1}`;
+  cardTimeline.to(card, {
     opacity: 1,
-    rotate: 0,
-    scale: 1,
     filter: 'blur(0px)',
-    scrollTrigger: {
-        trigger: ".design-cards",
-        pin: true,
+    stagger: 130,
+    duration: 10,
+    onComplete: () => {
+      //console.log('the card that was completed was', card, currentCard)
+      gsap.to(card, {
+        opacity: 0,
+        stagger: 5,
         scrub: true,
-        start: "-20%",
-        end: "5900px"
-    },
-    onComplete: self => {
-       $(this).fadeOut();
-  },
+        onComplete: () => {
+          console.log('Card faded out:', card);
+        }
+      });
+    }
+  });
 });*/
 // workspaces scroller end
 
+
+  //homepage design process gsock
+/*gsap.registerPlugin(ScrollTrigger);
+
+const cards = document.querySelectorAll(".desk-card");
+
+// Set the initial position of each card
+cards.forEach((card, index) => {
+  gsap.set(card, {
+    position: 'absolute',
+    opacity: 0,
+    filter: 'blur(16px)',
+    x: gsap.utils.random(-100, 100), // Randomly offset the X position
+    y: gsap.utils.random(-100, 100), // Randomly offset the Y position
+    scale: .3
+  });
+});
+
+const cardTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.design-cards', // The wrapper element that contains the panels
+    pin: true,
+    start: "-15%",
+    end: "+=600%",
+    scrub: true // Enable "scrubbing" so that the animations are smoothly reversed as the user scrolls back up
+  },
+});
+
+cards.forEach((card, index) => {
+  var currentCard = `${index + 1}`;
+  cardTimeline.to(card, {
+    filter: 'blur(0px)',
+    translateZ: 15,
+    scale: 1,
+    opacity: 1,
+    stagger: 1,
+    duration: 8,
+    onComplete: () => {
+      console.log(`Card ${index + 1} timeline completed`);    
+    }
+  });
+});*/
+
+
+window.onload = () => {
+  gsap.set("#scrollDist", {
+    width: "100%",
+    height: gsap.getProperty("#app", "height"),
+    onComplete: () => {
+      gsap.set("#app, #imgGroup", {
+        height: "100%",
+        width: "100%",
+        opacity: 1,
+        position: "fixed",
+        top: 0,
+        left: 0,
+        perspective: 300
+      });
+    }
+  });
+
+  gsap.set("#app img", {
+    position: "absolute",
+    attr: {
+      id: (iterator, target, array) => {
+        initImage(target, iterator);
+        return "img" + iterator;
+      }
+    }
+  });
+
+  const tl = gsap
+    .timeline({
+      defaults: {
+        duration: 1
+      },
+      //  onUpdate: ()=>{},
+      scrollTrigger: {
+        trigger: "#scrollDist",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1
+      }
+    })
+    .fromTo(
+      "#txt1",
+      {
+        scale: 0.4,
+        transformOrigin: "50%"
+      },
+      {
+        scale: 2,
+        opacity: 0.03
+      },
+      0
+    )
+    .to(
+      "#txt1 path",
+      {
+        duration: 0.3,
+        drawSVG: 0,
+        stagger: 0.05,
+        ease: "power1.in"
+      },
+      0
+    )
+    // Images
+    .fromTo(
+      ".imgBox",
+      { z: -5000 },
+      { z: 350, ease: "none", stagger: -0.3 },
+      0.3
+    )
+    .fromTo(".imgBox img", { scale: 3 }, { scale: 1.15, stagger: -0.3 }, 0.3)
+    .to(".imgBox", { duration: 0, pointerEvents: "auto", stagger: -0.3 }, 0.5)
+    .from(
+      ".imgBox img",
+      { duration: 0.3, opacity: 0, stagger: -0.3, ease: "power1.inOut" },
+      0.3
+    )
+    .to(
+      ".imgBox img",
+      { duration: 0.1, opacity: 0, stagger: -0.3, ease: "expo.inOut" },
+      1.2
+    )
+    .to(".imgBox", { duration: 0, pointerEvents: "none", stagger: -0.3 }, 1.27)
+    // End Text
+    .add("end")
+    .fromTo(
+      "#txt2",
+      {
+        scale: 0.1,
+        transformOrigin: "50%"
+      },
+      {
+        scale: 0.6,
+        ease: "power3"
+      },
+      "end-=0.2"
+    )
+    .from(
+      "#txt2 path",
+      {
+        duration: 0.4,
+        drawSVG: 0,
+        stagger: 0.15,
+        ease: "sine.inOut"
+      },
+      "end-=0.2"
+    );
+};
+
+function initImage(target, iterator) {
+  const box = document.createElement("div");
+  const imgGroup = document.querySelector("#imgGroup");
+  box.appendChild(target);
+  imgGroup.appendChild(box);
+  gsap.set(box, {
+    pointerEvents: "none",
+    position: "absolute",
+    attr: {
+      id: "box" + iterator,
+      class: "imgBox"
+    },
+    width: target.width,
+    height: target.height,
+    borderRadius: "24px",
+    overflow: "hidden",
+    scale: "1.2",
+    top: "50%",
+    left: "50%",
+    xPercent: -50,
+    yPercent: -50,
+    x: target.dataset.x,
+    y: target.dataset.y,
+    perspective: 500
+  });
+  
+  target.onmouseover = (e)=>{
+    $('#DeskAlt').html(e.srcElement.alt)    
+    $('#DeskAlt').css('opacity', 1);
+  }
+
+  target.onmouseout = (e)=>{
+    $('#DeskAlt').css('opacity', 0);
+  }
+}
+
+if (ScrollTrigger.isTouch === 1) {
+  gsap.set(".imgBox", { x: 0, y: 0 });
+} else {
+  window.addEventListener("mousemove", (e) => {
+    gsap.to(".imgBox", {
+      xPercent: (-e.clientX / innerWidth) * 100,
+      yPercent: -25 - (e.clientY / innerHeight) * 50,
+      rotateX: 4 - (e.clientY / innerHeight) * 8,
+      rotateY: -4 + (e.clientX / innerWidth) * 8
+    });
+    
+    gsap.to(".imgBox img", {
+      xPercent: (-e.clientX / innerWidth) * 10,
+      yPercent: -5 - (e.clientY / innerHeight) * 10
+    });
+    
+  });
+}
+
+//homepage design process gsock end
